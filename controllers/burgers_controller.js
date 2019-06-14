c4b47c70099cc5945d75505698af25de0830b304
Natasha Henry <natashaphenry@yahoo.com>
2019-06-14 13:48:38 -0400

    completed routes and config. burger not devoured will not post.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,11 +2,43 @@ var express = require('express');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
-router.get('/', function (req, res) {
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
     burger.all(function(burger_data) {
-        console.log(burger_data);
-        res.render('index');
-    })
-})
+      var hbsObject = {
+        burgers: burger_data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/burgers/insertOne", function(req, res) {
+    burger.insertOne([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function() {
+      res.redirect("/burgers");
+    });
+  });
+  
+  router.put("/burgers/updateOne/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.updateOne({
+      devoured: true
+    }, condition, function(response) {
+        res.sjson(response);
+      })
+    });
+
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,25 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-    all: function(cb){
-        orm.all('burgers', function(res){
+    //grabs all burgers from database
+    all: function (cb) {
+        orm.all('burgers', function (res) {
             cb(res);
         })
+    },
+    // add burgers to database
+    insertOne: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    //update burgers within database
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
     }
-}
+};
+
 
 module.exports = burger;
