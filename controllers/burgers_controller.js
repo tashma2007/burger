75f67fca06319f45c618550e75ef19939cddc75a
Natasha Henry <natashaphenry@yahoo.com>
2019-06-14 15:02:47 -0400

    updated routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,41 +4,41 @@ var burger = require('../models/burger.js');
 
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  res.redirect("/burgers");
+router.get("/", function (req, res) {
+  burger.selectAll(function (burger_data) {
+    var hbsObject = {
+      burgers: burger_data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
-router.get("/burgers", function(req, res) {
-    burger.all(function(burger_data) {
-      var hbsObject = {
-        burgers: burger_data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+//route to add burger
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne([
+    "burger_name"
+  ], [
+      req.body.name
+    ], function (result) {
+      res.json({ id: result.insertId });
     });
+});
+
+//route to update burger condition
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
-  
-  router.post("/burgers/insertOne", function(req, res) {
-    burger.insertOne([
-      "burger_name", "devoured"
-    ], [
-      req.body.burger_name, req.body.devoured
-    ], function() {
-      res.redirect("/burgers");
-    });
-  });
-  
-  router.put("/burgers/updateOne/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    burger.updateOne({
-      devoured: true
-    }, condition, function(response) {
-        res.sjson(response);
-      })
-    });
+});
 
 
 module.exports = router;
